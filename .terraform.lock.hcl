d099ac691f1737a415f94731ee272ca28da17509
Manuel Alba <malba@mmae.cl>
2023-02-22 03:09:50 -0300

    create elastic cluster and kibana

---
+++ .terraform.lock.hcl
@@ -0,0 +1,45 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/digitalocean/digitalocean" {
+  version     = "2.26.0"
+  constraints = "~> 2.0"
+  hashes = [
+    "h1:3STQ7zZHEv6PpjDveQTSHEfMrNWcrzs1bYAWoEZx0P4=",
+    "zh:10fc569e4669f1589b02e8d7e43398f2140872c3e74d11429ad74d7c0464176e",
+    "zh:402be0350250b133db11780704f26226a176ba6fa5016aef2be08f79e76edaa6",
+    "zh:506b886bf77ebad868283310dc886d40a793a98534dab7278eca826d0cdd7049",
+    "zh:745c82487f8fbfea15de975682bb6ba70ea79acfd783feeee9702a6b42e689c8",
+    "zh:85ed296ca9fe707afec0b2ca9263ef167e784e474a13d631c051fdbb90ad324d",
+    "zh:87d1eea592c70508df371793834e4aa73cc7cbd830ce8509950c26172734201e",
+    "zh:8c72fc549941c69aac988d2908d8922ea1a48338f73e8d2f8e5fb57fb6ac1197",
+    "zh:8d6b692e5351c92acf0bbc34865136ce7d961f82546c5cbe47f0050a088d46a8",
+    "zh:92cb56af41969755d803ff8d528f63012fa51e2c5e83861c1fd92df4dc680688",
+    "zh:9820f10523f7c6bca62c0eb12d8c923bf303846d679778158032e9c2e6ad29b6",
+    "zh:9ba0fe7fe519509acdc69d9d9d8a5bd0c06673d5730474ec15d6dfc81f790d31",
+    "zh:a670d7f7deae78285e469d8ff350629c8d34fdc6b0da05ff0b855c7846ff9342",
+    "zh:bb8bf49bdb163757b214c6f645b25e6a3eb9952ec6d1fb3dade56da33cd26460",
+    "zh:c347ed08e2b929718eb7c97406574b28c8e08a5c00068a40a9dd934ab487ef6b",
+    "zh:e4e423fdbcf1cc86c585738a0ea8a29413ac0e478dc338dee5594257a13f2fb3",
+    "zh:ee42adc96a03b94d0bdfd226bf691687d4b38e46eb81570674ae7a86cd58e3a6",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/null" {
+  version = "3.2.1"
+  hashes = [
+    "h1:ydA0/SNRVB1o95btfshvYsmxA+jZFRZcvKzZSB+4S1M=",
+    "zh:58ed64389620cc7b82f01332e27723856422820cfd302e304b5f6c3436fb9840",
+    "zh:62a5cc82c3b2ddef7ef3a6f2fedb7b9b3deff4ab7b414938b08e51d6e8be87cb",
+    "zh:63cff4de03af983175a7e37e52d4bd89d990be256b16b5c7f919aff5ad485aa5",
+    "zh:74cb22c6700e48486b7cabefa10b33b801dfcab56f1a6ac9b6624531f3d36ea3",
+    "zh:78d5eefdd9e494defcb3c68d282b8f96630502cac21d1ea161f53cfe9bb483b3",
+    "zh:79e553aff77f1cfa9012a2218b8238dd672ea5e1b2924775ac9ac24d2a75c238",
+    "zh:a1e06ddda0b5ac48f7e7c7d59e1ab5a4073bbcf876c73c0299e4610ed53859dc",
+    "zh:c37a97090f1a82222925d45d84483b2aa702ef7ab66532af6cbcfb567818b970",
+    "zh:e4453fbebf90c53ca3323a92e7ca0f9961427d2f0ce0d2b65523cc04d5d999c2",
+    "zh:e80a746921946d8b6761e77305b752ad188da60688cfd2059322875d363be5f5",
+    "zh:fbdb892d9822ed0e4cb60f2fedbdbb556e4da0d88d3b942ae963ed6ff091e48f",
+    "zh:fca01a623d90d0cad0843102f9b8b9fe0d3ff8244593bd817f126582b52dd694",
+  ]
+}
---
+++ main.tf
@@ -0,0 +1,209 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source = "digitalocean/digitalocean"
+      version = "~> 2.0"
+    }
+  }
+}
+
+variable "cluster-name" {
+  type = string
+  description = "The name of the cluster"
+}
+
+variable "cluster-elastic-nodes" {
+  type = number
+  description = "How many nodes to create for the cluster"
+}
+
+# Configure the DigitalOcean Provider
+provider "digitalocean" {
+  token = "<your Digital Ocean token>"
+}
+
+data "digitalocean_ssh_key" "ssh_key" {
+  name = "<your ssh key name>"
+}
+
+resource "digitalocean_droplet" "elastic" {
+  count  = var.cluster-elastic-nodes
+  image  = "ubuntu-22-04-x64"
+  name   = "${var.cluster-name}-elastic-search-${count.index+1}"
+  region = "sfo3"
+  size   = "s-2vcpu-2gb"
+  ssh_keys = [data.digitalocean_ssh_key.ssh_key.id]
+}
+
+
+resource "null_resource" "install_elasticsearch" {
+  count = var.cluster-elastic-nodes
+
+  connection {
+    type        = "ssh"
+    user        = "root"
+    private_key = file("~/.ssh/id_rsa")
+    host        = digitalocean_droplet.elastic[count.index].ipv4_address
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "curl -fsSL https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -",
+      "echo 'deb https://artifacts.elastic.co/packages/8.x/apt stable main' | sudo tee /etc/apt/sources.list.d/elastic-8.x.list",
+      "sudo apt update",
+      "sudo apt install -y elasticsearch",
+      "echo 'network.host: 0.0.0.0' >> /etc/elasticsearch/elasticsearch.yml",
+      "echo 'cluster.name: ${var.cluster-name}' >> /etc/elasticsearch/elasticsearch.yml"
+    ]
+  }
+}
+
+resource "null_resource" "configurate_elasticsearch_master" {
+  count = 1
+  depends_on = [
+        null_resource.install_elasticsearch
+        ]
+  connection {
+    type        = "ssh"
+    user        = "root"
+    private_key = file("~/.ssh/id_rsa")
+    host        = digitalocean_droplet.elastic[count.index].ipv4_address
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "sudo systemctl daemon-reload",
+      "sudo systemctl enable elasticsearch.service",
+      "sudo systemctl start elasticsearch.service",
+      "sleep 10",
+      "/usr/share/elasticsearch/bin/elasticsearch-create-enrollment-token -s kibana > /tmp/kibana-token.json",
+      "/usr/share/elasticsearch/bin/elasticsearch-create-enrollment-token -s node > /tmp/node-token.json",
+      "echo y | /usr/share/elasticsearch/bin/elasticsearch-reset-password -u elastic -s > /tmp/password.txt"
+    ]
+  }
+  provisioner "local-exec" {
+    command = "ssh-keyscan -H ${digitalocean_droplet.elastic[count.index].ipv4_address}>> ~/.ssh/known_hosts && scp root@${digitalocean_droplet.elastic[count.index].ipv4_address}:/tmp/kibana-token.json . && scp root@${digitalocean_droplet.elastic[count.index].ipv4_address}:/tmp/node-token.json . && scp root@${digitalocean_droplet.elastic[count.index].ipv4_address}:/tmp/password.txt ."
+  }
+}
+
+
+
+resource "null_resource" "enable_elasticsearch" {
+    depends_on = [
+        null_resource.configurate_elasticsearch_master
+    ]
+  count = "${var.cluster-elastic-nodes - 1}"
+
+  connection {
+    type        = "ssh"
+    user        = "root"
+    private_key = file("~/.ssh/id_rsa")
+    host        = "${digitalocean_droplet.elastic[count.index+1].ipv4_address}"
+  }
+  provisioner "file" {
+    source      = "node-token.json"
+    destination = "/tmp/token.json"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "token=`cat /tmp/token.json` && echo y | /usr/share/elasticsearch/bin/elasticsearch-reconfigure-node --enrollment-token $token",
+      "sudo systemctl daemon-reload",
+      "sudo systemctl enable elasticsearch.service",
+      "sudo systemctl start elasticsearch.service"
+    ]
+  }
+}
+
+
+
+resource "digitalocean_droplet" "kibana" {
+  count  = 1
+  image  = "ubuntu-22-04-x64"
+  name   = "${var.cluster-name}-kibana-${count.index+1}"
+  region = "sfo3"
+  size   = "s-2vcpu-2gb"
+  ssh_keys = [data.digitalocean_ssh_key.ssh_key.id]
+}
+
+
+resource "null_resource" "install_kibana" {
+  depends_on = [
+    null_resource.configurate_elasticsearch_master
+  ]
+  count = 1
+  connection {
+    type        = "ssh"
+    user        = "root"
+    private_key = file("~/.ssh/id_rsa")
+    host        = digitalocean_droplet.kibana[count.index].ipv4_address
+  }
+  provisioner "file" {
+    source      = "kibana-token.json"
+    destination = "/tmp/token.json"
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "curl -fsSL https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -",
+      "echo 'deb https://artifacts.elastic.co/packages/8.x/apt stable main' | sudo tee /etc/apt/sources.list.d/elastic-8.x.list",
+      "sudo apt update",
+      "sudo apt install -y kibana",
+      "sudo sed -i 's/#server.host: \"localhost\"/server.host: \"0.0.0.0\"/' /etc/kibana/kibana.yml",
+      "echo 'elasticsearch.hosts: [\"${join("\",\"", digitalocean_droplet.elastic.*.ipv4_address)}\"]' >> /etc/kibana/kibana.yml",
+      "sudo systemctl enable kibana",
+      "sudo systemctl start kibana",
+      "token=`cat /tmp/token.json` && /usr/share/kibana/bin/kibana-setup --enrollment-token $token"
+
+    ]
+  }
+}
+
+
+resource "digitalocean_droplet" "apm-server" {
+  count  = 1
+  image  = "ubuntu-22-04-x64"
+  name   = "${var.cluster-name}-apm-server-${count.index+1}"
+  region = "sfo3"
+  size   = "s-2vcpu-2gb"
+  ssh_keys = [data.digitalocean_ssh_key.ssh_key.id]
+}
+
+
+output "Kibana_URL" {
+    depends_on = [
+      null_resource.configurate_elasticsearch_master,
+        null_resource.install_kibana
+    ]
+  value       = "wait 2 minute to connect to http://${digitalocean_droplet.kibana[0].ipv4_address}:5601"
+  description = "Now you can connect to kibana with this url:"
+}
+
+output "Password" {
+    depends_on = [
+      null_resource.configurate_elasticsearch_master,
+        null_resource.install_kibana
+    ]
+  value       = "Your elastic password can found in password.txt file"
+  description = "Password elastic is: "
+}
+
+
+# # # Load Balancer
+# # resource "digitalocean_loadbalancer" "elasticsearch_lb" {
+# #   name = "${var.cluster-name}-load-balancer"
+# #     region = "sfo3"
+
+# #   forwarding_rule {
+# #     entry_port     = 9200
+# #     entry_protocol = "tcp"
+
+# #     target_port     = 9200
+# #     target_protocol = "tcp"
+# #   }
+
+# #   healthcheck {
+# #     port     = 9200
+# #     protocol = "tcp"
+# #   }
+
+# #   droplet_ids = digitalocean_droplet.elastic.*.id
+# # }
+
